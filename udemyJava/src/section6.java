126555c40da3f5cc1c6bbb9b9d4d78ac05ef10e1
Shwz.ssilva <juniorsz.7013@gmail.com>
2026-02-02 19:12:19 -0300

    01

---
+++ udemyJava/src/section6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class section6   {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a senha: ");
+            int senha = sc.nextInt();
+
+            while (senha != 2002){
+                System.out.println("Senha invalida!");
+                senha = sc.nextInt();
+            }
+        System.out.println("Acesso Permitido");
+        sc.close();
+    }
+}
